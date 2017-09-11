77224f0d89b87b40c3e8e2c178e938e72d6e0e27
zhangml123 <s15532292192@163.com>
2017-09-11 10:46:24 +0800

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -43,7 +43,8 @@ module.exports = function(env){
           },
         ],
 
-      }, 
+      },  
+	  
        resolve: {
                 modules: [
                     path.resolve(root_dir, "app"),
